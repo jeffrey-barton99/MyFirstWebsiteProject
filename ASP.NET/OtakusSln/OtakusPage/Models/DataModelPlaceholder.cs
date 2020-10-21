82a4f1b4a0f8cb1b03bbd0b5c3784aec7e7db72d
Jeffrey Barton <jeffrey.barton99@outlook.com>
2020-10-20 22:56:37 -0500

    Data model spacing fixed in line's 13

--- ASP.NET/OtakusSln/OtakusPage/Models/DataModelPlaceholder.cs
+++ ASP.NET/OtakusSln/OtakusPage/Models/DataModelPlaceholder.cs
@@ -11,7 +11,8 @@ namespace OtakusPage.Models
         public long ProductID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        [Column(TypeName = "decimal(8, 2)")] public decimal Price { get; set; }
+        [Column(TypeName = "decimal(8, 2)")] 
+        public decimal Price { get; set; }
         public string Category { get; set; }
     }
 }
