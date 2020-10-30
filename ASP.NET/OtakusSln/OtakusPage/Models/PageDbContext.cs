b3667daf9c5ec544bf80192d4070289b27b4a946
Jeffrey Barton <jeffrey.barton99@outlook.com>
2020-10-30 16:17:11 -0500

    Added and populated PageDbContext.cs to the Models folder.

---
+++ ASP.NET/OtakusSln/OtakusPage/Models/PageDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace OtakusPage.Models
+{
+    public class PageDbContext : DbContext
+    {
+        public PageDbContext(DbContextOptions<PageDbContext> options) : base(options) { }
+        
+        public DbSet<Form> Form { get; set; }
+        public DbSet<AccountCreationDate> AccountCreationDate { get; set; }
+        public DbSet<UserProfile> UserProfile { get; set; }
+
+    }
+}
